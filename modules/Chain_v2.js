a4b0fd0609ef858413ba864b4c3c2c38e90be109
leeenx <leeenx@users.noreply.github.com>
2017-10-10 21:17:45 -0500

    添加 sort 方法与 indexOf 方法

--- modules/Chain_v2.js
+++ modules/Chain_v2.js
@@ -165,8 +165,8 @@ export default class Chain {
 				prevNode[NEXT] = node; 
 			}
 			// POINTER指向HEAD，头指针更新为当前节点
-			else {
-				this.HEAD = node; 
+			else { 
+				this.setHead(node); 
 			}
 			// 链表长度增加1
 			++this.length; 
@@ -188,8 +188,8 @@ export default class Chain {
 				nextNode[PREV] = node; 
 			}
 			// POINTER指向TAIL，尾指针更新为当前节点
-			else {
-				this.TAIL = node; 
+			else { 
+				this.setTail(node); 
 			}
 			// 保持datas的插入顺序
 			currNode = node; 
@@ -233,6 +233,72 @@ export default class Chain {
 	reverse() { 
 		[this.HEAD, this.TAIL, this.NEXT, this.PREV] = [this.TAIL, this.HEAD, this.PREV, this.NEXT]; 
 		return true; 
+	}
+	// 排序
+	sort(compare) {
+		// 快速排序函数
+		let quickSort = (node, len, compare = (a, b) => a - b) => { 
+			// 基点 node
+			let pivot = node, 
+			// 计数
+			count = 0, 
+			// 左侧节点长度，默认为1
+			leftLen = 1,
+			// 当前片段的最左侧节点 
+			leftNode; 
+			while(++count < len) { 
+				node = node.next; 
+				// 当前元素大于基点值，转移到基点左侧
+				if(compare(pivot.data, node.data) > 0) { 
+					// node 的前驱与后续
+					let next = node.next, prev = node.prev; 
+					// 移除 node
+					if(next !== null) {
+						// 非尾节点
+						[next.prev, prev.next] = [prev, next]; 
+					}
+					else {
+						// 尾节点
+						prev.next = next; 
+						this.setTail(prev); 
+					}
+					// 在 pivot 前插入 node
+					if(pivot.prev !== null) {
+						// pivot 非头节点
+						[pivot.prev.next, pivot.prev, node.prev, node.next] = [node, node, pivot.prev, pivot]; 
+					}
+					else {
+						// 头节点
+						[pivot.prev, node.prev, node.next] = [node, pivot.prev, pivot]; 
+						this.setHead(node); 
+					}
+					// 最左侧节点标记
+					leftNode === undefined && (leftNode = node); 
+					// 左侧节点增加1
+					++leftLen; 
+					// node 回退到后继
+					node = prev; 
+				}
+			}
+			// 分割后递归
+			leftLen > 1 && quickSort(leftNode, leftLen, compare); 
+			len - leftLen > 1 && quickSort(pivot.next, len - leftLen, compare); 
+		}
+		// 链表指针回退至头部 
+		quickSort(this.HEAD, this.length, compare); 
+	}
+	// 索引
+	indexOf(data, fromIndex = 0) {
+		this.at(fromIndex); 
+		let targetIndex = fromIndex; 
+		while(this.TAIL !== this.POINTER) { 
+			let curData = this.next(); 
+			if(curData === data) {
+				return targetIndex; 
+			}
+			++targetIndex; 
+		} 
+		return -1; 
 	}
 	// 克隆链表的一段切片
 	slice(start = 0, end) { 
@@ -295,13 +361,13 @@ export default class Chain {
 	setHead(node = null) {
 		this.HEAD = node; 
 		// 如果尾指针未指定，头尾指针同指向node
-		this.TAIL === null && this.setTail(node); 
+		this.TAIL === null && this.setTail(node) & this.setPointer(node); 
 	}
 	// 指定尾指针
 	setTail(node = null) {
 		this.TAIL = node; 
 		// 如果头指针未指定，头尾指针同指向node
-		this.HEAD === null && this.setHead(node); 
+		this.HEAD === null && this.setHead(node) & this.setPointer(node);  
 	}
 	// 指定指针
 	setPointer(node = this.HEAD) {
@@ -320,5 +386,4 @@ export default class Chain {
 			that.POINTER = that.TAIL; 
 		}()); 
 	}
-
 }
